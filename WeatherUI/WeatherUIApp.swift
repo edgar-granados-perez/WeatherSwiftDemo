ea9a354231292a4d09cc0bb29f1fdc6e3b7fa121
holounix <60943198+holounix@users.noreply.github.com>
2021-05-29 02:36:45 -0500

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Edgar Granados-Perez on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
